b31e58a12c0b0f00537871cee5a56869387dd2b2
Ankur Charan <ankurcharan98@gmail.com>
2019-09-09 17:10:45 +0530

    adding component
    Signed-off-by: Ankur Charan <ankurcharan98@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Switch, BrowserRouter, Route, Link } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import HomePage from './components/HomePage/HomePage';
+
+class App extends Component {
+	
+	render() {
+		return (
+			<BrowserRouter>
+			<div>
+			
+			<Switch>
+			
+				{/** Add you componenrs here */}
+				<Route exact path='/' component={HomePage} />
+			</Switch>
+			
+			</div>
+			</BrowserRouter>
+		);
+	}
 }
 
 export default App;
+	
---
+++ src/components/HomePage/HomePage.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class HomePage extends Component {
+	
+	render() {
+		return (
+			<div>
+
+				<h1>HomePAge hai ey</h1>
+
+			</div>
+		);
+	}
+}
+
+export default HomePage;
+	
