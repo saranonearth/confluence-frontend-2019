2cdce98a48cdb6dd056d76c0ddaa04cad3191d50
saranonearth <saran.suresh@outlook.com>
2019-09-15 21:45:32 +0530

    Client-Side Oauth implemented

--- src/actions/gallery.js
+++ src/actions/gallery.js
@@ -1,1 +1,13 @@
 import actionTypes from './actionTypes';
+
+//actions using dispatch 
+
+
+//example:
+// export const login = (dispatch) => {
+
+//     dispatch({
+//         type: actionTypes.LOGIN
+//     })
+
+// }
--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,12 +1,13 @@
 import React, { useContext } from 'react';
 import Store from '../../store/store';
-
+import OauthLogin from '../utils/OauthLogin';
 const HomePage = () => {
   const { state, dispatch } = useContext(Store);
   console.log(state);
   return (
     <>
       <h1>Home</h1>
+      <OauthLogin />
     </>
   );
 };
