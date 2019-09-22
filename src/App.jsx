e455ad0a4ad3cf3046a334e974e5eb1d33e4494a
Ankur Charan <ankurcharan98@gmail.com>
2019-09-22 23:54:32 +0530

    Revert "Team Conflu"

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,6 @@ import LoggedIn from './LoggedIn';
 import Error404 from './components/Error404/Error404';
 import Store from './store/store';
 import rootReducer from './reducers/rootReducer';
-import TeamConlfu from './components/TeamConflu/TeamConflu'
-import TeamConflu from './components/TeamConflu/TeamConflu';
 
 const App = () => {
   const initState = useContext(Store);
@@ -28,7 +26,6 @@ const App = () => {
             <LoggedIn path='/guestlectures' component={Guests} />
             <LoggedIn path='/contact' component={Contact} />
             <LoggedIn path='/about' component={AboutUs} />
-            <LoggedIn path='/teamConflu' component={TeamConflu} />
             <LoggedIn path='/sponsors' component={Sponsors} />
             <LoggedIn path='/user' component={User} />
             <LoggedIn component={Error404} />
