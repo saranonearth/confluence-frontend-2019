f4e0fc40dd3538bf5250d4c4ba52373ae6517861
saranonearth <saran.suresh@outlook.com>
2019-09-30 23:11:12 +0530

    cleared old code and installed axios

--- src/components/Events/Categories.jsx
+++ src/components/Events/Categories.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import Nav from '../utils/Nav';
 
 const Categories = () => {
   return (
-    <div class='container'>
-      <Nav />
+    <div>
+      <h1>Categories</h1>
     </div>
   );
 };
+
+export default Categories;
