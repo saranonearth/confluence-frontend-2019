6081a4c38d930858f281e4f5be87d08d20c19334
saranonearth <saran.suresh@outlook.com>
2019-10-02 02:35:02 +0530

    category fetched

--- src/components/Events/Categories.jsx
+++ src/components/Events/Categories.jsx
@@ -1,7 +1,22 @@
-import React from 'react';
-import Nav from '../utils/Nav';
-
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 const Categories = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const getEvents = async () => {
+      try {
+        const res = await axios.get(
+          'http://confluence-backend.appspot.com/api/events/name/'
+        );
+
+        setData([...res.data.data]);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getEvents();
+  }, []);
+  console.log(data);
   return (
     <>
       <h1>Categories</h1>
