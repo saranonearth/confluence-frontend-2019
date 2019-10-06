d023526e54c63777f630c7a6b3e3d5fd092fd76c
ashu <ayushgupta2305@gmail.com>
2019-10-07 03:04:33 +0530

    about us, need edit, resolving conflict
    Signed-off-by: ashu <ayushgupta2305@gmail.com>

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
-
+import Console from "./console.png"
 const AboutUs = () => {
   return (
     <>
+      <div className="console">
+        <img src={Console} alt="Console" />
+        
+      </div>
       <h1>AboutUs</h1>
     </>
   );
