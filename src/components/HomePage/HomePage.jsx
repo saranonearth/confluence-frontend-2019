f70687b3fe15ea50fc8d3c17637d8248204be00a
ashu <ayushgupta2305@gmail.com>
2019-10-06 12:47:48 +0530

    home page and nav bar

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -7,7 +7,18 @@ const HomePage = () => {
   console.log(state);
   return (
     <>
-      <div class='container'>
+      <div className='main'>
+      <div className="container">
+                <div class="contain">
+                    <div class="rotator">
+                        <img id="disc" src="discElements.png" alt="" />
+                        <img id="conflutxt" src="smalldisk.png" alt="" />
+                    </div>
+                        <img id="theme" src="homepagetheme.png" alt="" />
+                        
+                        
+                </div>
+        </div>
         <Nav />
       </div>
     </>
--- src/components/utils/Nav.jsx
+++ src/components/utils/Nav.jsx
@@ -1,63 +1,26 @@
 import React, { useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 const Nav = () => {
-  var menu = useRef(null);
-  useEffect(() => {
-    menu = document.querySelector('#toggle');
-    const menuItems = document.querySelector('#overlay');
-    const menuContainer = document.querySelector('.menu-container');
-    const menuIcon = document.querySelector('i');
-    const tape = document.querySelector('.tape');
-    const front = document.querySelector('.front');
-    function toggleMenu(e) {
-      tape.classList.toggle('hide');
-      front.classList.toggle('hide');
-      menuItems.classList.toggle('open');
-      menuContainer.classList.toggle('full-menu');
-      menuIcon.classList.toggle('fa-bars');
-      menuIcon.classList.add('fa-times');
-      e.preventDefault();
-    }
-
-    menu.addEventListener('click', toggleMenu, false);
-  }, []);
-
-  const menuList = [
-    {
-      route: '/',
-      nav: 'Home'
-    },
-    {
-      route: '/categories',
-      nav: 'Events'
-    },
-    {
-      route: '/sponsors',
-      nav: 'Sponsors'
-    },
-    {
-      route: '/ask',
-      nav: 'Ask Queries'
-    }
-  ];
+  
   return (
     <>
-      <div class='menu-container' id='toggle'>
-        <a href='#' ref={menu} class='menu'>
-          <i class='fa fa-bars' aria-hidden='true'></i>
-        </a>
-      </div>
-      <div class='overlay' id='overlay'>
-        <nav class='overlay-menu'>
-          <ul>
-            {menuList.map((m, i) => (
-              <li key={i}>
-                <Link to={m.route}>{m.nav}</Link>
-              </li>
-            ))}
-          </ul>
-        </nav>
-      </div>
+      <div class="outer-menu">
+        <input class="checkbox-toggle" type="checkbox" />
+        <div class="hamburger">
+            <div></div>
+        </div>
+        <div class="menu">
+            <div>
+                <ul>
+                <li><a href="#" data-text="Home">Home</a></li>
+                <li><a href="#" data-text="About us">About Us</a></li>
+                <li><a href="#" data-text="Sponsors">Sponsors</a></li>
+                <li><a href="#" data-text="Contact">Contact</a></li>
+                <li><a href="#" data-text="Developers">Developers</a></li>
+                </ul>
+            </div>
+        </div>
+        </div>
     </>
   );
 };
