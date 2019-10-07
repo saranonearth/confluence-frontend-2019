051381b5ec52c9b3d125cee46f5c4d39e0fb8c01
ashu <ayushgupta2305@gmail.com>
2019-10-07 16:41:32 +0530

    sponsors updated

--- src/components/Sponsors/Sponsors.jsx
+++ src/components/Sponsors/Sponsors.jsx
@@ -1,9 +1,38 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import Nav from '../utils/Nav';
+import axios from 'axios';
+import config from '../../config.json';
+import sponsorHeading from "./SponsorsFont.svg";
+import './sponsors.css';
+import title from "./bulb.jpg";
+import online from "./online.jpg";
 const Sponsors = () => {
   return (
     <>
-      <h1>Sponsors</h1>
+    <div className="sponsor-container">
+      <Nav />
+        <img className="sponsor-head" src={sponsorHeading} alt="sponsorHeading" />
+      
+      <div className="sponsors-cont">
+        <div className="title">
+          <h1 className="title-head"> TITLE SPONSOR </h1>
+          {/* card started for titlee sponsor */}
+          <img className="title-img" src={title} alt="titleImg" />
+          <h3 className="title-name"> TITLE NAME </h3>
+        </div>
+        <div className="online">
+          <h1 className="online-head"> ONLINE PATNERS </h1>
+          <div className="online-sponsors">
+               {/* cards started for online patners */}
+              <div className="online-single">
+                <img className="online-img" src={online} alt="" />
+                <h2 className="online-name" > ONLINE-NAME </h2>
+              </div>
+          </div> 
+        </div> 
+      </div> 
+    </div> 
+      
     </>
   );
 };
