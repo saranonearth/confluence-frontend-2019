51c2e56bc714aeb097ef11ea10f06839ac140b3c
saranonearth <saran.suresh@outlook.com>
2019-10-07 12:48:38 +0530

    register fixed

--- src/components/utils/NotAuth.jsx
+++ src/components/utils/NotAuth.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import Nav from '../utils/Nav';
 const NotAuth = () => {
   return (
     <div>
-      <h1>You are not authenticated</h1>
+      <Nav />
+
+      <div className='category-container'>
+        <div className='title-holder'></div>
+
+        <div className='c-container'>
+          <p className='text'>
+            You are not loggedin. Please login from home page.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
