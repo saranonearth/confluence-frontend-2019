499a565afbc68eb1b8ce8d38ea4b55d55a5e733d
ashu <ayushgupta2305@gmail.com>
2019-10-08 17:48:28 +0530

    fixed about us
    Signed-off-by: ashu <ayushgupta2305@gmail.com>

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import aboutUsText from "./AboutusText.png";
 import frontScreen from "./frontScreen.png";
-import Console from "./console.png";
+import Console from "./AboutusConsiletext.png";
 import backGlow from "./pinkglow.png";
 import './AboutUs.css';
 import Nav from '../utils/Nav';
@@ -10,11 +10,9 @@ const AboutUs = () => {
     <>
     <div className="about-container wd">
       <div className="console">
-        <img className="aboutustext" src={aboutUsText} alt="aboutUsText" /> 
-        <img className="aboutusscreen" src={frontScreen} alt="frontScreen" />
-        <img className="console-pic" src={Console} alt="Console" />
+             <img className="console-pic" src={Console} alt="Console" />
         
-        <img className="backglow" src={backGlow} alt="backGlow" />
+        
       </div>
       <p class="aboutuscontent">
       Confluence, the annual cultural Festival of NIT Kurukshetra is a cultural extravaganza beyond words, beyond description. Confluence is the coming together of free spirits, like-minded zealous youth and a celebration of culture of true artistic genius. Its urge to deliver the maximum is what makes it one of the most awaited cultural festival.
